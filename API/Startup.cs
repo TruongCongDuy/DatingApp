243305078b42e783097f843f03338abb99807827
TruongCongDuy <truongcongduy1406@gmail.com>
2022-06-21 16:49:04 +0700

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -10,16 +10,22 @@ using Microsoft.OpenApi.Models;
 
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
     public class Startup
     {
-        private readonly IConfiguration _confi;
+        private readonly IConfiguration _config;
        
-        public Startup(IConfiguration confi)
+        public Startup(IConfiguration config)
         {
-            _confi = confi;
+            _config = config;
            
             
         }
@@ -29,15 +35,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options=>
-            {
-                options.UseSqlite(_confi.GetConnectionString("DefaultConnection"));
-
-            });
+           services.AddApplicationServices(_config);
         
 
             services.AddControllers();
             services.AddCors();
+           services.AddIdentityServices(_config);
             // services.AddSwaggerGen(c =>
             // {
             //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -58,8 +61,9 @@ namespace API
 
             app.UseRouting();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseAuthentication();
 
-            app.UseAuthorization();
+            app.UseAuthorization(); 
 
             app.UseEndpoints(endpoints =>
             {
