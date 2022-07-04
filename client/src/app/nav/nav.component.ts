57ccdbdb308b1babfc828b1ea77f35c0b7fd2ca0
TruongCongDuy <truongcongduy1406@gmail.com>
2022-07-04 11:00:49 +0700

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -28,11 +28,7 @@ export class NavComponent implements OnInit {
       {
         this.router.navigateByUrl('/members');
         
-      },error=>{
-        console.error(error);
-        this.toastr.error(error.error);
-      }
-    
+      }   
       )
   }
   logout()
